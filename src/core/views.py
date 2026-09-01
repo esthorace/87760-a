499aa3d11bbced03d22bf48869d5eed0543b9e1c
Esteban <esteban.horacio.acevedo@gmail.com>
2026-08-31 21:33:06 -0300

    Crea primer template

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    datos_a_plantilla = {"titulo": "EducaciónIt", "año": 2026}
+    return render(request, "core/index.html", context=datos_a_plantilla)
+
+
+def saludar(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hola desde Django")
